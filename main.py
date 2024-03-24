e9ce985354daa2e9ec58c0de898681a5fe86d087
Michael Lawrence <mjl93@me.com>
2024-03-24 03:37:15 -0700

    Fixed CSV loading function for package class and moved it to package.py.

--- main.py
+++ main.py
@@ -1,33 +1,15 @@
 import csv
+import hashtable
+from package import load_package_data
 
-from hashtable import HashTable
-from package import Package
-from truck import Truck
-import data
-
-
-def loadpackagedata(HashTable):
-    with open("data/Packages.csv") as package_CSV:
-        package_data = csv.reader(package_CSV, delimiter=',')
-        for package in package_data:
-            pid = int(package[0])
-            paddress = package[1]
-            pcity = package[2]
-            pstate = package[3]
-            pzip = package[4]
-            pdeadline = package[5]
-            pkilos = package[6]
-            pnotes = package[7]
-
-            p = Package(pid, paddress, pcity, pstate, pzip, pdeadline,
-                        pkilos, pnotes)
-            HashTable.insert(pid, p)
+# Read the package information
+with open("data/Packages.csv") as packageCSV:
+    packageData = csv.reader(packageCSV)
+    packageData = list(packageData)
 
 
-with open("data/Address.csv") as address_CSV:
-    address_data = csv.reader(address_CSV, delimiter=',')
-    address_data = list(address_data)
-
-
-
-
+# code for testing
+ht = hashtable.HashTable()
+load_package_data("data/Packages.csv", ht)
+print(ht)
+print(ht.search('18'))
--- package.py
+++ package.py
@@ -1,12 +1,13 @@
-# Class for the packages to be delivered
+import csv
+
 
 class Package:
-    def __init__(self, ID, address, city, state, zip, deadline, kilos, notes):
+    def __init__(self, ID, address, city, state, zipcode, deadline, kilos, notes):
         self.ID = ID
         self.address = address
         self.city = city
         self.state = state
-        self.zip = zip
+        self.zipcode = zipcode
         self.deadline = deadline
         self.kilos = kilos
         self.notes = notes
@@ -14,6 +15,27 @@ class Package:
         self.arrival_time = None
 
     def __str__(self):
-        return "%s, %s, %s, %s, %s, %s, %s, %s, %s %s" % (self.ID, self.address, self.city, self.state, self.zip,
+        return "%s, %s, %s, %s, %s, %s, %s, %s, %s %s" % (self.ID, self.address, self.city, self.state, self.zipcode,
                                                           self.deadline, self.kilos, self.notes, self.departure_time,
                                                           self.arrival_time)
+
+
+# Function for populating hashtable with Package data and creating objects from that data
+def load_package_data(filename, ht):
+    with open(filename, encoding='utf-8-sig') as package_CSV:
+        package_data = csv.reader(package_CSV, delimiter=',')
+
+        for package in package_data:
+            pid = package[0]
+            paddress = package[1]
+            pcity = package[2]
+            pstate = package[3]
+            pzipcode = package[4]
+            pdeadline = package[5]
+            pkilos = package[6]
+            pnotes = package[7]
+
+            p = Package(pid, paddress, pcity, pstate, pzipcode, pdeadline,
+                        pkilos, pnotes)
+
+            ht.insert(pid, p)
