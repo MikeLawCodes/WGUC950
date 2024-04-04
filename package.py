60bc9985a194c997f7495f2f9b5da29a68d5d6d2
Michael Lawrence <mjl93@me.com>
2024-04-03 17:49:39 -0700

    Added function to enact status updates for package objects

--- package.py
+++ package.py
@@ -11,7 +11,7 @@ class Package:
         self.zipcode = zipcode
         self.deadline = deadline
         self.kilos = kilos
-        self.status = status
+        self.status = ShippingStatus.AT_HUB
         self.departure_time = None
         self.in_transit_time = None
         self.arrival_time = None
@@ -20,12 +20,19 @@ class Package:
     def __str__(self):
         return "%s, %s, %s, %s, %s, %s, %s, %s, %s %s %s" % (
             self.package_id, self.package_address, self.city, self.state, self.zipcode,
-            self.deadline, self.kilos, self.status, self.departure_time, self.in_transit_time,
+            self.deadline, self.kilos, self.status.name, self.departure_time, self.in_transit_time,
             self.arrival_time,)
 
+    # TODO
+    # convert_time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
 
-# TODO
-# convert_time = datetime.timedelta(hours=int(h), minutes=int(m), seconds=int(s))
+    def update_status(self, convert_timedelta):
+        if self.arrival_time < convert_timedelta:
+            self.status = ShippingStatus.DELIVERED
+        elif self.departure_time > convert_timedelta:
+            self.status = ShippingStatus.ON_TRUCK
+        else:
+            self.status = ShippingStatus.AT_HUB
 
 
 # enumeration for packages_on_truck status
